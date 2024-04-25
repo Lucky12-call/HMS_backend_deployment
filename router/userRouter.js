1fee1c71a8a8b2bcfdf0ca654c06782de2b41284
Lucky <lk899202@gmail.com>
2024-04-25 12:41:30 +0530

    post request fixed

--- router/userRouter.js
+++ router/userRouter.js
@@ -24,6 +24,6 @@ router.get("/admin/me", isAdminAuthenticated, getUserDetails);
 router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
 router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
-router.post("/doctor/admin", isAdminAuthenticated, addNewDoctor);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
 
 export default router;
